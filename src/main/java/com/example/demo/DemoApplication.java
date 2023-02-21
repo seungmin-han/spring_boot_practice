123d73427eee31e3264e2afe1c5494568c432725
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-02-21 15:52:55 +0900

    Feat: File Upload 기능 구현

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,16 @@
 package com.example.demo;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.domain.TempDTO;
 import com.example.demo.service.TempService;
@@ -30,6 +33,18 @@ public class DemoApplication {
 	@GetMapping("/temp")
 	public ArrayList<TempDTO> temp() {
 		return tempService.getItems();
+	}
+
+	@PostMapping("/file")
+	public File fileUpload(@RequestParam(value = "files") MultipartFile[] files) {
+		File file = new File(files[0].getOriginalFilename());
+		try {
+			files[0].transferTo(file);
+		} catch(Exception exception) {
+			System.err.println(exception);
+		}
+		
+		return file;
 	}
 
 }
